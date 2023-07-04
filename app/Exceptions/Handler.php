937a38dfec5cc750a86ca7e32eb622a00413fe45
Nur Faizi <nurfaizee76@gmail.com>
2023-07-04 13:39:33 +0700

    Initial commit

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception  $throwable
+     * @return void
+     */
+    public function report(Throwable $throwable)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($throwable)) {
+            app('sentry')->captureException($throwable);
+        }
+
+        parent::report($throwable);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $throwable
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $throwable)
+    {
+        return parent::render($request, $throwable);
+    }
+}
---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,8 @@
+<link
+  rel="stylesheet"
+  href="{{ url('frontend/libraries/bootstrap/css/bootstrap.css') }}"
+/>
+<link
+    href="https://fonts.googleapis.com/css2?family=Urbanist:wght@200;400;700;800&display=swap" rel="stylesheet"
+/>
+<link rel="stylesheet" href="{{ url('frontend/styles/main.css') }}" />
